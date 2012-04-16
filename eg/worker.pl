f195e2ec169beb329d865273d4c064d4bf2fff6b
Chris Prather <chris@prather.org>
2012-04-16 15:46:46 -0400

    add the `$channel->ack()` to the callback.

--- eg/worker.pl
+++ eg/worker.pl
@@ -2,9 +2,17 @@
 use 5.12.2;
 use Crixa;
 
-my $mq = Crixa->connect( host => 'localhost' );
-my $q = $mq->queue( name => 'task_queue', durable => 1 );
+my $mq      = Crixa->connect( host => 'localhost' );
+my $channel = $mq->channel;
+my $q       = $channel->queue( name => 'task_queue', durable => 1 );
 
-$q->handle_message( sub { say $_->{body}; sleep( $_->{body} =~ y/.// ); } );
+$q->handle_message(
+    sub {
+        say $_->{body};
+        sleep( $_->{body} =~ y/.// );
+        say ' [x] Done';
+        $channel->ack($_->{delivery_tag} );
+    }
+);
 
 __END__
