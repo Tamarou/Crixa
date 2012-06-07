e1e303172fc7f1e8bc9af295994e3b0ed9c685a8
Chris Prather <chris@prather.org>
2012-06-07 19:33:35 -0400

    move fix a missing reference maker

--- eg/receive_logs_topic.pl
+++ eg/receive_logs_topic.pl
@@ -10,7 +10,7 @@ my $exchange
 my @binding_keys = @ARGV;
 die "Usage: $0 [info] [warning] [error]" unless @binding_keys;
 
-my $q = $exchange->queue( exclusive => 1, bindings => @binding_keys );
+my $q = $exchange->queue( exclusive => 1, bindings => \@binding_keys );
 
 say ' [*] Waiting for logs. To exit press CTRL+C';
 $q->handle_message( sub { say "$_->{routing_key} $_->{body}" } );
